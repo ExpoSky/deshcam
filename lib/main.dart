b040dfc8570c269cb5879ea4a6b6a80267fa2d70
Furkan Kabasakal <furkan@vearshop.com>
2024-08-16 23:58:34 +0300

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:deshcam/constants/routes.dart';
 import 'package:deshcam/services/auth/auth_service.dart';
 import 'package:deshcam/views/login_view.dart';
-import 'package:deshcam/views/notes_view.dart';
+import 'package:deshcam/views/notes/new_note_view.dart';
+import 'package:deshcam/views/notes/notes_view.dart';
 import 'package:deshcam/views/register_view.dart';
 import 'package:deshcam/views/verify_email.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized;
 
@@ -22,6 +22,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
